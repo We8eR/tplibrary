93b079858d8aae7c3b2c77662badf09aeecab46c
We8eR <122805348+We8eR@users.noreply.github.com>
2024-08-28 21:39:51 +0300

    ...

--- tplibrary/tplibrary/urls.py
+++ tplibrary/tplibrary/urls.py
@@ -18,15 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-urlpatterns += [
-    path('importtp/', include('importtp.urls')),
-]
-
-urlpatterns += [
-    path('', RedirectView.as_view(url='/importtp/', permanent=True)),
-]
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='catalog/')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
