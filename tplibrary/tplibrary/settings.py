4ddb021189d7780cb430ed4c3a3ee0d1064b249f
We8eR <122805348+We8eR@users.noreply.github.com>
2024-10-08 22:47:18 +0300

    .

--- tplibrary/tplibrary/settings.py
+++ tplibrary/tplibrary/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'importtp.apps.ImpotrttpConfig',
+    'impotrttp.apps.ImpotrtpConfig',
 ]
 
 MIDDLEWARE = [
--- tplibrary/tplibrary/urls.py
+++ tplibrary/tplibrary/urls.py
@@ -22,7 +22,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', admin.site.urls),
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='catalog/')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
